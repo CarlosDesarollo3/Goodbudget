ced343d1c652e6434064118ff9bb5eb838424afb
CarlosDesarollo3 <desarrollo3@cnet.co.cr>
2026-02-17 20:34:43 -0600

    fix: polyfill FormData in app entrypoint

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { registerRootComponent } from 'expo';
+import FormData from 'react-native/Libraries/Network/FormData';
 import App from './App';
+
+if (globalThis.FormData == null) {
+  globalThis.FormData = FormData;
+}
 
 registerRootComponent(App);
