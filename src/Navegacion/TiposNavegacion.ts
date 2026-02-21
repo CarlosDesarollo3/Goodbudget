4f78ba814e00e8cca0c7717a5f3516fab0a10fe3
CarlosDesarollo3 <desarrollo3@cnet.co.cr>
2026-02-21 13:39:52 -0600

    Mejora FAB rapido y agrega altas de cuenta/grupo

--- src/Navegacion/TiposNavegacion.ts
+++ src/Navegacion/TiposNavegacion.ts
@@ -1,19 +1,19 @@
 import { NavigatorScreenParams } from '@react-navigation/native';
-import { Transaccion } from '@/Dominio/Modelos';
+import { TipoTransaccion, Transaccion } from '@/Dominio/Modelos';
 
 export type ParametrosPestanasPrincipal = {
-  PantallaInicio: undefined;
+  PantallaInicio: { accionRapida?: 'cuenta' | 'grupo' } | undefined;
   PantallaCategorias: undefined;
   PantallaAnalitica: undefined;
 };
 
 export type ParametrosNavegacion = {
   PantallaPestanasPrincipal: NavigatorScreenParams<ParametrosPestanasPrincipal> | undefined;
-  PantallaInicio: undefined;
+  PantallaInicio: { accionRapida?: 'cuenta' | 'grupo' } | undefined;
   PantallaDetalleGrupo: { idGrupo: string; nombreGrupo: string };
   PantallaDetalleCuenta: { idCuenta: string; nombreCuenta: string };
   PantallaFormularioTransaccion:
-    | { idCuentaPredeterminada?: string }
+    | { idCuentaPredeterminada?: string; idCategoriaPredeterminada?: string; tipoPredeterminado?: TipoTransaccion }
     | { transaccion?: Transaccion; duplicar?: boolean }
     | undefined;
   PantallaCategorias: undefined;
